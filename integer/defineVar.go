4662e880015d17caacf1859a4b4de8a929add57b
a.kumar <a.kumar@medlabmg.com>
2018-05-25 07:45:57 +0200

    added basic and multi intger variable handling

---
+++ integer/defineVar.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+   var age int //variable declaration
+
+   fmt.Println("my age is", age, "\n")
+   
+   age = 26 // assign value
+   fmt.Println("my age is", age, "\n")
+
+   age = 56 * 2 // reassign value
+   fmt.Println("my age is", age, "\n")
+}
---
+++ integer/multiVar.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+    var width, height int //Defining integer type variables
+    
+    fmt.Println("width is", width, "and height is", height)
+
+    width = 100
+    
+    height = 80
+
+    fmt.Println(" new width is", width, "new height is", height)
+}
